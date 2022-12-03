cdf21353cab708f9338fc0096319142987af8e10
Mamont257 <108954365+Mamont257@users.noreply.github.com>
2022-12-03 11:47:15 +0200

    task-5_done

---
+++ js/task-05.js
@@ -0,0 +1,19 @@
+const input = document.querySelector('#name-input');
+const span = document.querySelector('#name-output');
+
+input.addEventListener('input', onInput);
+
+function onInput(evt) {
+    if (evt.currentTarget.value) {
+        span.textContent = evt.currentTarget.value;
+    } else {
+        span.textContent = 'Anonymous';
+    }
+}
+
+
+
+
+// Напиши скрипт, який під час набору тексту в інпуті input#name - input(подія input),
+// підставляє його поточне значення в span#name - output.Якщо інпут порожній,
+// у спані повинен відображатися рядок "Anonymous".
