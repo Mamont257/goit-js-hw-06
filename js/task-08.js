47525699f71a01609080068d8933873a0a0f5fed
Volodymyr Zankevych <108954365+Mamont257@users.noreply.github.com>
2022-12-03 16:56:57 +0200

    task-8_done+-

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,57 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', onSubmit);
+const user ={};
+
+function onSubmit(event){
+    event.preventDefault();
+    // console.dir(event.currentTarget.elements.email.value);
+    const {email, password} = event.currentTarget.elements;
+    // console.dir(password.value);
+    // console.dir(event.currentTarget[1].value);
+    // console.log(event)
+    // const {value} = event.currentTarget[0];
+    // const {password} = event.currentTarget[1].value;
+
+
+
+    if(!email.value || !password.value){
+        console.log(alert('Всі поля повинні бути заповненні!!!'));
+    } else {
+        user.email = email.value;
+        user.password = password.value;
+        console.log(user);
+    }
+
+
+
+
+
+
+
+    // if(!event.currentTarget[0].value || !event.currentTarget[1].value){
+    //     console.log(alert('Всі поля повинні бути заповненні!!!'));
+    // } else {
+    //     user.email = event.currentTarget[0].value;
+    //     user.password = event.currentTarget[1].value;
+    //     console.log(user);
+    // }
+}
+
+
+
+
+
+
+
+
+
+
+
+// 1. Обробка відправлення форми form.login-form повинна відбуватися відповідно до події submit.
+// 2. Під час відправлення форми сторінка не повинна перезавантажуватися.
+// 3. Якщо у формі є незаповнені поля, виводь alert з попередженням про те, що всі поля повинні бути заповнені.
+// 4. Якщо користувач заповнив усі поля і відправив форму, збери значення полів в об'єкт, 
+//    де ім'я поля буде ім'ям властивості, а значення поля - значенням властивості. 
+//    Для доступу до елементів форми використовуй властивість elements.
+// 5. Виведи об'єкт із введеними даними в консоль і очисти значення полів форми методом reset.
